f2bdc9a1b8c8417d12dd7e28b7b6acf2dbbd95a6
Christopher Adams <christopher.r.adams@gmail.com>
2011-05-21 20:57:11 -0400

    Django admin added.

---
+++ lib/Wyclif/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from Wyclif.models import Profile,Chapter,Paragraph,Title,Author,Page
+
+admin.site.register(Profile)
+admin.site.register(Chapter)
+admin.site.register(Paragraph)
+admin.site.register(Title)
+admin.site.register(Author)
+admin.site.register(Page)
