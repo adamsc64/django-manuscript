056fd1c357239bea24390959d9db278d3a313737
Christopher Adams <christopher.r.adams@gmail.com>
2011-05-21 20:56:42 -0400

    Urls and views.

--- lib/Wyclif/urls.py
+++ lib/Wyclif/urls.py
@@ -2,52 +2,79 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 from django.http import HttpResponse
 
-from django.contrib import admin
-admin.autodiscover()
+urlpatterns = patterns('')
 
-from views import index
+urlpatterns += patterns('',
+	(r'^/?$', 'Wyclif.views.index'),
+	(r'^input/?$', 'Wyclif.views.input'),
+)
 
-# implementing tastypie api.
-from tastypie.api import Api
-#from Nexus.outwardapi import TransactionResource, UserResource
-
-v1_api = Api(api_name='v1')
-#v1_api.register(TransactionResource())
+urlpatterns += patterns('',
+	(r'^sentry/', include('sentry.urls')),
+)
 
 
-urlpatterns = patterns('',
-	(r'^/?$', index),
-	
-	(r'api/', include(v1_api.urls)),
-    (r'^sentry/', include('sentry.urls')),
-#	(r'^googled05e050f361f6ab1\.html$', 'Nexus.views.googleproof'),
-	(r'^robots\.txt$', 'robotstxt'),
+#static media
+urlpatterns += patterns('',
 	(r'^static/(?P<path>.*)$',			'django.views.static.serve',
 										{
 											'document_root':	settings.STATIC_PATH,
-											'show_indexes':		True
+											'show_indexes':		(settings.DEBUG==True)
 										}
 	),
+	(r'^media/(?P<path>.*)$',			'django.views.static.serve',
+										{
+											'document_root':	settings.MEDIA_ROOT,
+											'show_indexes':		(settings.DEBUG==True)
+										}
+	),
+	(r'^robots\.txt$', lambda response : HttpResponse("User-agent: *\r\nDisallow: /\r\n", mimetype="text/plain") ),
+)
 
-#	(r'^media/(?P<path>.*)$',			'django.views.static.serve',
-#										{
-#											'document_root':	settings.ADMIN_MEDIA_LOC,
-#											'show_indexes':		True
-#										}
-#	),
+# implementing tastypie api.
+from tastypie.api import Api
+from Wyclif.api import ParagraphResource, ChapterResource, TitleResource
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+v1_api = Api(api_name='v1')
+v1_api.register(ParagraphResource())
+v1_api.register(ChapterResource())
+v1_api.register(TitleResource())
 
-    # Uncomment the next line to enable the admin:
-   	(r'^admin/',						include(admin.site.urls)),
+urlpatterns += patterns('',
+	(r'^api/', include(v1_api.urls)),
+)
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns += patterns('',
+	(r'^admin/', include(admin.site.urls)),
+    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
 	(r'^accounts/login/', 				'django.contrib.auth.views.login', {'template_name': 'django/registration/login.html'}),
 	(r'^accounts/logout/', 				'django.contrib.auth.views.logout', {'next_page': '/'})	,
 )
 
-if settings.DEBUG==False:
-	urlpatterns += patterns('',
-	   	(r'^.*$',						'Nexus.views.do404',)
-	)
+# implementing /db/ views.
+
+from Wyclif.bin.csv_to_db import run_view
+from Wyclif.models import Paragraph, Title, Author
+
+urlpatterns += patterns('',
+
+	(r'^db/runimport/$', run_view),
+	(r'^db/paragraphs/$', 'django.views.generic.list_detail.object_list', {
+		'queryset' : Paragraph.objects.all(),
+		'template_name' : 'Wyclif/object_list.html'
+	}),
+#		'extra_context' : { 'fieldnames' : Paragraph._meta.get_all_field_names() }
+	(r'^db/titles/$', 'django.views.generic.list_detail.object_list', {
+		'queryset' : Title.objects.all(),
+		'template_name' : 'Wyclif/object_list.html'
+	}),
+	(r'^db/authors/$', 'django.views.generic.list_detail.object_list', {
+		'queryset' : Author.objects.all(),
+		'template_name' : 'Wyclif/object_list.html'
+	}),
+
+)
--- lib/Wyclif/views.py
+++ lib/Wyclif/views.py
@@ -1,16 +1,77 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404, HttpResponseRedirect, HttpResponseNotFound
 
-#def googleproof(request):
-#	return HttpResponse('google-site-verification: googled05e050f361f6ab1.html')
+from Wyclif.forms import ParagraphForm, TitleForm, PageForm, ChapterForm
 
-def robotstxt(request):
-	return HttpResponse("User-agent: *\r\nDisallow: /\r\n")
-	
-def do404(request):
-	#raise Http404()
-	#return HttpResponseNotFound()
-	return HttpResponseRedirect('/')
 
 def index(request):
 	return render_to_response('Wyclif/index.html')
+	
+def input(request):
+	#request_variables = dict(request.REQUEST.items())
+	
+	return render_to_response('Wyclif/input.html')
+
+def input_title(request):
+	title_form = TitleForm()
+
+	return render_to_response('Wyclif/input.html', {
+		'title_form' : title_form,
+	})
+
+def input_chapter(request):
+	chapter_form = ChapterForm()
+
+	return render_to_response('Wyclif/input.html', {
+		'chapter_form' : chapter_form,
+	})
+
+def input_page(request):
+	paragraph_form = ParagraphForm()
+	page_form = PageForm()
+
+	return render_to_response('Wyclif/input.html', {
+		'paragraph_form' : paragraph_form,
+		'page_form' : page_form,
+	})
+
+def edit_title(request, id):
+	try:
+		title = Title.objects.get(id=id)
+	except Title.DoesNotExist:
+		raise Http404
+
+	title_form = TitleForm(title)
+
+	return render_to_response('Wyclif/edit.html', {
+		'title_form' : title_form,
+	})
+
+def edit_chapter(request, id):
+	try:
+		chapter = Chapter.objects.get(id=id)
+	except Chapter.DoesNotExist:
+		raise Http404
+
+	chapter_form = ChapterForm(chapter)
+
+	return render_to_response('Wyclif/edit.html', {
+		'chapter_form' : chapter_form,
+	})
+
+def edit_page(request, id):
+	try:
+		page = Page.objects.get(id=id)
+	except Page.DoesNotExist:
+		raise Http404
+
+	page_form = PageForm(page)
+	paragraphs = Paragraph.objects.filter(page = page)
+	paragraph_forms = []
+	for paragraph in paragraphs:
+		paragraph_forms.append(ParagraphForm(paragraph))
+		
+	return render_to_response('Wyclif/edit.html', {
+		'paragraph_forms' : paragraph_forms,
+		'page_form' : page_form,
+	})
