3c38121a13c4169918bef1b80b792b192a077f43
Christopher Adams <christopher.r.adams@gmail.com>
2011-06-25 22:42:21 -0400

    Made admin look nicer.

--- lib/wyclif/admin.py
+++ lib/wyclif/admin.py
@@ -2,9 +2,28 @@ from django.contrib import admin
 
 from wyclif.models import Chapter,Paragraph,Title,Author,Page
 
-#admin.site.register(Profile)
-admin.site.register(Chapter)
-admin.site.register(Paragraph)
+admin.site.register(Chapter,
+	list_display = ('title','heading','start_page_no'),
+	list_display_links = ('heading',),
+	search_fields = ['heading','start_page_no'],
+	
+	fields = ('title','heading','start_page_no','get_children_links'),
+	readonly_fields = ('get_children_links',),
+)
+admin.site.register(Paragraph,
+	list_display = ('chapter','number','page','split','text'),
+	list_display_links = ('number','text'),
+	search_fields = ['number','text'],
+)
 admin.site.register(Title)
 admin.site.register(Author)
-admin.site.register(Page)
+
+admin.site.register(Page,
+	list_display = ('title','number','scan'),
+	list_display_links = ('number',),
+	list_filter = ('title',),
+	search_fields = ['number',],
+
+	fields = ('title','number','scan','get_children_links'),
+	readonly_fields = ('get_children_links',),
+)
