995f899cdf11264972b79f6e3e190a36c73a2f48
Christopher Adams <christopher.r.adams@gmail.com>
2011-07-31 17:36:33 -0400

    Removed extra 'author' references, simplified links to chapters.

--- wyclif/works/urls.py
+++ wyclif/works/urls.py
@@ -9,7 +9,6 @@ urlpatterns = patterns('',
 	url(r'^(?P<title>[-\w]+)/$', views.chapters, name="show-chapters"),
 	url(r'^(?P<title>[-\w]+)/(?P<page>\d+)/$', views.page, name="show-page"),
 	url(r'^(?P<title>[-\w]+)/(?P<chapter>[-\w]+)/$', views.chapter, name="show-chapter"),
-	url(r'^(?P<title>[-\w]+)/(?P<chapter>[-\w]+)/$', views.paragraphs, name="show-paragraphs"),
 #	url(r'^(?P<title>.*)/(?P<model>.*)/?$', 				'model_by_work'),
 #	url(r'^(?P<title>.*)/(?P<model>.*)/(?P<id>\d*)/?$', 	'element_by_id'),
 )
