911cd8aaa936e38373591b8497a4ca175e1cbb06
Christopher Adams <christopher.r.adams@gmail.com>
2011-11-05 16:24:02 -0400

    Convert tabs to spaces.

--- manuscript/models.py
+++ manuscript/models.py
@@ -218,31 +218,31 @@ class Page(BaseModel):
                 #self.convert_scan_to_display() # avoids infinite loop.
                 pass # do nothing for now until we figure out jpeg decoder on server.
     
-	def get_normalized_jpg_filename(self):
-		return str(self.title.slug) + "_p" + str(self.number) + ".jpg"
-	
-	def convert_scan_to_display(self, commit=True):
-		if hasattr(self.scan,"path"): # If scan exists.
-			if not hasattr(self.display,"path"): # But display doesn't.
-				(width , height) = (self.scan.width , self.scan.height)
-		
-				target_width = settings.MANUSCRIPT_DEFAULT_WIDTH
-				if target_width > width:				
-    			    # Don't make it bigger. Only make it smaller.
-    			    target_width = width
-    			    target_height = height
-    			else:
-    				target_height = int(1.0 * target_width / width * height)
-    			    
-				target_size = (target_width , target_height)
-				target_path = settings.MEDIA_ROOT + "pages_to_display/" + self.get_normalized_jpg_filename()
-		
-				print "%s: Converting %s %s file to %s %s (new file)." % (str(self), str(self.scan.path), str((width,height)), str(target_path), str(target_size))
-				PIL.Image.open(self.scan.path).resize(target_size).save(target_path)
-		
-				self.display = "pages_to_display/" + self.get_normalized_jpg_filename()
-				if commit:
-					self.save()
+    def get_normalized_jpg_filename(self):
+        return str(self.title.slug) + "_p" + str(self.number) + ".jpg"
+    
+    def convert_scan_to_display(self, commit=True):
+        if hasattr(self.scan,"path"): # If scan exists.
+            if not hasattr(self.display,"path"): # But display doesn't.
+                (width , height) = (self.scan.width , self.scan.height)
+        
+                target_width = settings.MANUSCRIPT_DEFAULT_WIDTH
+                if target_width > width:                
+                    # Don't make it bigger. Only make it smaller.
+                    target_width = width
+                    target_height = height
+                else:
+                    target_height = int(1.0 * target_width / width * height)
+                    
+                target_size = (target_width , target_height)
+                target_path = settings.MEDIA_ROOT + "pages_to_display/" + self.get_normalized_jpg_filename()
+        
+                print "%s: Converting %s %s file to %s %s (new file)." % (str(self), str(self.scan.path), str((width,height)), str(target_path), str(target_size))
+                PIL.Image.open(self.scan.path).resize(target_size).save(target_path)
+        
+                self.display = "pages_to_display/" + self.get_normalized_jpg_filename()
+                if commit:
+                    self.save()
 
 
 class Paragraph(BaseModel):
