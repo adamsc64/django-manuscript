38a0e02d4a70b7b992a0bbe99cd69d38ed9358ff
Christopher Adams <christopher.r.adams@gmail.com>
2011-11-06 16:11:51 -0500

    Create CSS class search-entry-field and bind search query validation to any element with it as a class. Move javascript from html to its own manuscript-search.js file.

--- manuscript/forms.py
+++ manuscript/forms.py
@@ -2,9 +2,11 @@ from django import forms
 from manuscript.models import Paragraph, Title, Page, Chapter
 
 class BigSearchForm(forms.Form):
-	q = forms.CharField(max_length=1000, label="Search for text")
-	titles = forms.ModelMultipleChoiceField(queryset=Title.objects_with_data.all(), label="In", required=False)
-	nearprompt = forms.IntegerField(min_value=1, max_value=100, required=False)
+	q = forms.CharField(max_length=1000, label="What are you looking for?",
+	    widget=forms.TextInput(attrs={"class":"search-entry-field"}),
+	)
+	titles = forms.ModelMultipleChoiceField(queryset=Title.objects_with_data.all(), label="Where are you looking for it?", required=False)
+	nearprompt = forms.IntegerField(min_value=1, max_value=100, required=False, label="Near by how many words?")
 
 	def clean_q(self):
 		q = self.cleaned_data["q"]
---
+++ manuscript/static/manuscript/js/manuscript-search.js
@@ -0,0 +1,52 @@
+(function(){
+
+	$(document).ready(function(){
+
+        // Enable functionality for nice looking multi-select.
+        
+		$("#search-tool select").chosen();
+
+		// todo: This needs to be made better!
+		var $main_search_input = $("#id_q");
+		var $near_prompt_wrap = $("#near-prompt-wrap");
+		var $near_prompt = $("#id_nearprompt");
+		function check_near_prompt() {
+			var text = $main_search_input.val();
+			if (text && text.search("NEAR") > -1) {
+				$near_prompt_wrap.fadeIn(500);
+				if ($near_prompt.val() == "") {
+					$near_prompt.val("5");
+				}
+			} else {
+				$near_prompt_wrap.fadeOut(500);
+				$near_prompt.val("");
+			}
+		}
+		check_near_prompt();			
+		$main_search_input.keyup(check_near_prompt);
+
+        // Enable search help link functionality
+
+        var $search_help = $("#search-help-link");
+        var $search_instructions = $(".search-instructions-wrap")
+        $search_help.click(function(e) {
+            e.preventDefault();
+            $search_help.hide();
+            $search_instructions.slideDown(1000);
+        });
+        
+        // Limit search input to valid characters
+
+        var $search_entry_fields = $(".search-entry-field");
+        $search_entry_fields.keypress(function(e) {
+            var code = (e.keyCode ? e.keyCode : e.which);
+            var key = String.fromCharCode(code);
+            var regex_invalid = /[^A-Za-z0-9\s\*]/;
+            if (key.match(regex_invalid)) {
+                e.preventDefault();
+            }
+        });
+
+    });
+
+})();
