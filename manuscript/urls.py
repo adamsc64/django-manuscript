bdde8bb3ee9a79dc0ae777bb6e226dbc2be18dfb
Christopher Adams <christopher.r.adams@gmail.com>
2011-11-16 13:53:20 -0500

    Add sample url patterns to views for management functions.

--- manuscript/urls.py
+++ manuscript/urls.py
@@ -12,6 +12,10 @@ urlpatterns = patterns('',
 	url(r'^(?P<title>[-\w]+)/(?P<chapter>[-\w]+)/$', 'manuscript.views.chapter', name="show-chapter"),
 #	url(r'^(?P<title>.*)/(?P<model>.*)/?$', 				'model_by_work'),
 #	url(r'^(?P<title>.*)/(?P<model>.*)/(?P<id>\d*)/?$', 	'element_by_id'),
+
+#   url(r'^img_to_db/run/$', 'wyclif.bin.img_to_db.run_view'),
+#   url(r'^db/runimport/$', 'wyclif.bin.csv_to_db.run_view'),
+
 )
 
 
